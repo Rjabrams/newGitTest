6ad8fb15a210635d72384971e98b4a7a69dc28c8
raquel abrams <rjabrams@ucsc.edu>
2016-01-23 15:25:54 -0800

    added addition.java and index.html

---
+++ addition.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Addition{
+		
+	public static void main(String[] args){
+
+	Scanner input = new Scanner(System.in);
+
+	int firstNumber = 0;, secondNumber = 0;
+	int sum;
+
+	System.out.println("Enter First Number:");
+	firstNumber = input.nextInt();
+	System.out.println("Enter Second Number:");
+	secondNumber = input.nextInt();
+
+	sum = firstNumber + secondNumber;
+
+	System.out.println("Sum of the two number is:" + sum);
+	}
+}
